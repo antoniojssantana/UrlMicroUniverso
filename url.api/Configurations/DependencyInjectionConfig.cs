9d502a91a5518b501abceaf03ab264e4ce4a8eaf
Antonio J S Santana <antonio.santana@sgatec.com.br>
2021-07-20 21:57:33 -0300

    atualizacao api

---
+++ url.api/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using sga.utils;
+using sga.utils.Interfaces;
+using url.api.Interfaces;
+using url.api.Services;
+using url.business.Interfaces.Repository;
+using url.business.Interfaces.Services;
+using url.business.Notifications;
+using url.business.Services;
+using url.data.Context;
+using url.data.Repository;
+
+namespace url.api.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependecies(this IServiceCollection services)
+        {
+            //Repositories
+            services.AddScoped<URLDbContext>();
+
+            services.AddScoped<IUserBaseRepository, UserBaseRepository>();
+
+            //Services
+            services.AddScoped<IUserBaseService, UserBaseService>();
+
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IEmailSender, EmailService>();
+
+            //API
+            services.AddScoped<INotifier, Notifier>();
+
+            return services;
+        }
+    }
+}
--- url.data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace url.data
-{
-    public class Class1
-    {
-    }
-}
